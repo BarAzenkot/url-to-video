acb769d517ce05b4155ea3adb1b8502c7585e732
BarAzenkot <79269066+BarAzenkot@users.noreply.github.com>
2023-03-22 10:00:50 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# url-to-video
+
+# URL to MP4
+
+A generator that gets a JSON File with url, makes a screenshot, convert it to a 10 seconds video and returns its path.
+
+
+
+
+
+## Usage
+
+if you wish to invoke the lambda function run:
+
+```cURL
+curl --location --request POST 'https://n22pwco6a4.execute-api.eu-west-1.amazonaws.com/bigvu-url-to-video-lambda' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "url": "https://bigvu.tv"
+}'
+```
+
+you can change the url to whatever you wish.
+## Tech Stack
+
+
+**Server:** aws-sdk, puppeteer-core, fluent-ffmpeg, @sparticuz/chrome-aws-lambda.
+
+**Cloud:** API-gateway, lambda, IAM, s3 bucket. 
+
--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const bucket = "bigvu-bar-azenkot-mp4-bucket";
 
 const s3 = new AWS.S3();
 
-const badRequestResponse = {
-  statusCode: 400,
-  body: "invalid url",
-};
-
 const isValidURL = (url) => {
   try {
     const parsedURL = new URL(url);
@@ -28,66 +23,76 @@ const makeVideo = async (url) => {
 
   // Check if url is valid
   if (!isValidURL(url)) {
-    return badRequestResponse;
+    return (badRequestResponse = {
+      statusCode: 400,
+      body: "Invalid url",
+    });
   }
 
-  // Launch Puppeteer and browser
-  const executablePath = await chromium.executablePath;
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath,
-    headless: chromium.headless,
-  });
+  try {
+    // Launch puppeteer and browser
+    const executablePath = await chromium.executablePath;
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath,
+      headless: chromium.headless,
+    });
 
-  // Navigate to the website
-  const page = await browser.newPage();
-  await page.goto(url);
+    // Navigate to the website
+    const page = await browser.newPage();
+    await page.goto(url);
 
-  // Take a screenshot
-  const screenshot = await page.screenshot({ type: "png" });
-  fs.writeFileSync(screenshotPath, screenshot);
-  const imageData = fs.readFileSync(screenshotPath);
+    // Take a screenshot
+    const screenshot = await page.screenshot({ type: "png" });
+    fs.writeFileSync(screenshotPath, screenshot);
+    const imageData = fs.readFileSync(screenshotPath);
 
-  // Close the browser when done
-  await browser.close();
+    // Close the browser when done
+    await browser.close();
 
-  // Create a video
-  ffmpeg()
-    .input(fs.createReadStream(screenshotPath))
-    .loop(Infinity)
-    .duration(10)
-    .videoCodec("libx264")
-    .outputOptions("-pix_fmt", "yuv420p")
-    .outputOptions("-movflags", "+faststart")
-    .outputOptions("-vf", "scale=1280:-2")
-    .outputFormat("mp4")
-    .on("error", (e) => {
-      console.error("An error occurred: " + e.message);
-    })
-    .on("end", () => {
-      console.log("Video converting has finished successfully!");
-    })
-    .pipe(fs.createWriteStream(videoPath), { end: true });
+    // Create a video
+    ffmpeg()
+      .input(fs.createReadStream(screenshotPath))
+      .loop(Infinity)
+      .duration(10)
+      .videoCodec("libx264")
+      .outputOptions("-pix_fmt", "yuv420p")
+      .outputOptions("-movflags", "+faststart")
+      .outputOptions("-vf", "scale=1280:-2")
+      .outputFormat("mp4")
+      .on("error", (e) => {
+        console.error("An error occurred: " + e.message);
+      })
+      .on("end", () => {
+        console.log("Video converting has finished successfully!");
+      })
+      .pipe(fs.createWriteStream(videoPath), { end: true });
 
-  // Upload video
-  const key = `${uuid.v4()}.png`;
-  await s3
-    .upload({
-      Bucket: bucket,
-      Key: key,
-      Body: imageData,
-      ContentType: "image/x-png",
-    })
-    .promise();
+    // Upload video
+    const key = `${uuid.v4()}.png`;
+    await s3
+      .upload({
+        Bucket: bucket,
+        Key: key,
+        Body: imageData,
+        ContentType: "image/x-png",
+      })
+      .promise();
 
-  // Return video path
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      path: `https://${bucket}.s3.eu-west-1.amazonaws.com/${key}`,
-    }),
-  };
+    // Return video path
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        path: `https://${bucket}.s3.eu-west-1.amazonaws.com/${key}`,
+      }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message,
+    };
+  }
 };
 
 exports.handler = async (event) => {
